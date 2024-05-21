be14e90fde246a65ccb44114a93b4870841aac5b
DaoodS <m.daood.shaheer@gmail.com>
2024-05-21 22:40:21 +0530

    house_robbers

---
+++ house_robbers/solution.py
@@ -0,0 +1,30 @@
+# Time Limit exceeded 48/70
+class Solution(object):
+    def rob(self, nums):
+        totalR = [-1]*len(nums)
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums)==1:
+            return nums[0]
+        elif len(nums)==2:
+            return max(nums)
+
+        def getReward(n):
+            if n==(len(nums)-1):
+                return nums[len(nums)-1]
+            
+            if n+2<len(nums):
+                if totalR[n+2]==-1:
+                    totalR[n+2] = getReward(n+2)
+            if n+1<len(nums):
+                if totalR[n+1]==-1:
+                    totalR[n+1] = getReward(n+1)
+
+            if (n+2)>(len(nums)-1):
+                return max((nums[n]), getReward(n+1))
+            else:
+                return max((nums[n] + getReward(n+2)), getReward(n+1))
+            
+        return getReward(0)
