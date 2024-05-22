2e4d7637a0594760aad9cb1c722070fe81649df0
DaoodS <m.daood.shaheer@gmail.com>
2024-05-22 13:16:47 +0530

    cc

--- house_robbers/solution.py
+++ house_robbers/solution.py
@@ -14,17 +14,13 @@ class Solution(object):
         def getReward(n):
             if n==(len(nums)-1):
                 return nums[len(nums)-1]
+            elif n==(len(nums)-2):
+                return max(nums[len(nums)-1], nums[len(nums)-2])
             
-            if n+2<len(nums):
-                if totalR[n+2]==-1:
-                    totalR[n+2] = getReward(n+2)
-            if n+1<len(nums):
-                if totalR[n+1]==-1:
-                    totalR[n+1] = getReward(n+1)
-
-            if (n+2)>(len(nums)-1):
-                return max((nums[n]), getReward(n+1))
-            else:
-                return max((nums[n] + getReward(n+2)), getReward(n+1))
+            if totalR[n+2]==-1:
+                totalR[n+2] = getReward(n+2)
+            if totalR[n+1]==-1:
+                totalR[n+1] = getReward(n+1)
+            return max((nums[n] + getReward(n+2)), getReward(n+1))
             
         return getReward(0)
